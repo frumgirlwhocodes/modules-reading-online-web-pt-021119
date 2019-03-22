6218d072a1ae600753fd1eb0d5b58403dee93be9
lieby weintraub <liebahinda@gmail.com>
2019-03-22 17:58:34 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -1,17 +1,17 @@
 require_relative './class_methods_module.rb'
 require_relative './dance_module.rb'
-require_relative './fancy_dance.rb'
-#class Dancer
-  #include Dance
-   #extend MetaDancing
- 
- # attr_accessor :name
- 
-#  def initialize(name)
- #   @name = name
-#  end
-#end 
+#require_relative './fancy_dance.rb'
 class Dancer
-  extend FancyDance::ClassMethods
-  include FancyDance::InstanceMethods
-end
+  include Dance
+   extend MetaDancing
+ 
+  attr_accessor :name
+ 
+  def initialize(name)
+    @name = name
+  end
+end 
+#class Dancer
+ # extend FancyDance::ClassMethods
+#  include FancyDance::InstanceMethods
+#end
